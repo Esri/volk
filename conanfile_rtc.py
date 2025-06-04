6569778f7b4b7bef334b5aba76f2d3d13cc4e970
Julia Lubiszewska <jlubiszewska@esri.com>
2025-06-04 15:53:15 +0100

    Update version number

--- conanfile_rtc.py
+++ conanfile_rtc.py
@@ -5,7 +5,7 @@ from conans import ConanFile
 
 class VolkConan(ConanFile):
     name = "volk"
-    version = "1.3.285"
+    version = "1.4.316"
     url = "https://github.com/Esri/volk/blob/runtimecore/"
     license = "https://github.com/Esri/volk/blob/runtimecore/LICENSE.md"
     description = ("Volk is a meta-loader for Vulkan")
